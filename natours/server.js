8303ab6093c0425530f1a9315be010e8f2571ad7
Koigor97 <turaysamuel101@yahoo.com>
2023-11-30 22:42:20 +0200

    save data in mongodb test, passed

--- natours/server.js
+++ natours/server.js
@@ -10,31 +10,47 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-main().catch((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('DB connected successfully...');
-  }
-});
-
-async function main() {
-  await mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  });
-}
+  })
+  .then(() => console.log());
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log());
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The quick brown Fox',
+  rating: 5.7,
+  price: 40,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Starting the server and listening for requests
 const port = process.env.PORT || 3000;
