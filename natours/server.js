ca60282ae25971e0921b8baeb5d7405f845dc3aa
Koigor97 <turaysamuel101@yahoo.com>
2023-11-30 22:24:33 +0200

    modify sserver.js

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,40 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+// connecting mongoose to the database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+main().catch((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('DB connected successfully...');
+  }
+});
+
+async function main() {
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+}
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log());
 
 // Starting the server and listening for requests
 const port = process.env.PORT || 3000;
