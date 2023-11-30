fbeb400809678a30a7765c3f2bd28c9d5507b89c
Koigor97 <turaysamuel101@yahoo.com>
2023-11-30 23:14:04 +0200

    modify server.js

--- natours/server.js
+++ natours/server.js
@@ -17,55 +17,7 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log());
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The quick brown Fox',
-  rating: 5.7,
-  price: 40,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-const testTour2 = new Tour({
-  name: 'Jungle excursion',
-  rating: 7.7,
-  price: 100,
-});
-
-testTour2
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log('DB connection started....'));
 
 // Starting the server and listening for requests
 const port = process.env.PORT || 3000;
