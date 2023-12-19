fbd23a6983f7b8fd2aa9ee90b4d913bf5f972020
Koigor97 <turaysamuel101@yahoo.com>
2023-12-19 23:36:03 +0200

    modify node-farm index.js file

--- node-farm/index.js
+++ node-farm/index.js
@@ -13,23 +13,13 @@ const fillInCardTemplate = require("./modules/replaceTemplate");
 // fs.writeFileSync("./txt/output.txt", textOut);
 
 // Non-blocking asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   console.log(data1);
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         if (err) console.log("An error occured saving the file");
-//         else console.log("The file has been saved");
-//       });
-//     });
-//   });
-// });
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return;
+  const dataObject = JSON.parse(data1);
+  console.log(dataObject);
+});
 
 // console.log("Testing...");
-
 //////////////////////////////////////////////////////////////
 // SERVER
 
