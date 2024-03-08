b9f81f9ebb6629e3ae08347ab17698a2f1662a67
Koigor97 <turaysamuel101@yahoo.com>
2024-03-08 18:12:00 +0200

    create a post route

--- natours/app.js
+++ natours/app.js
@@ -21,7 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
 
 //handling post request to route
 app.post('/api/v1/tours', (req, res) => {
-  res.status(201).json({ message: 'post was successfully created' });
+  // creating temporary id
+  const newId = tours[tours.length - 1].id + 1;
+  //getting the data sent
+  const dataRecieved = req.body[0];
+  console.log(newId, dataRecieved);
+
+  // creating a new data
+  const newPost = { id: newId, ...dataRecieved };
+  tours.push(newPost);
+
+  // saving data to the tours-simple.json file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res
+        .status(201)
+        .json({ message: 'post was successfully created', postSent: newPost });
+    }
+  );
 });
 
 const port = 8000;
