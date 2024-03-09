fee12c188164a72c8eccbbce2b138a854e1cabd2
Koigor97 <turaysamuel101@yahoo.com>
2024-03-09 18:39:11 +0200

    create userController js file

---
+++ natours/controllers/userController.js
@@ -0,0 +1,39 @@
+exports.getAllUsers = (req, res) => {
+  res.status(505).json({
+    status: 'error',
+    result: 'This route is not yet defined',
+  });
+  return;
+};
+
+exports.getUser = (req, res) => {
+  res.status(505).json({
+    status: 'error',
+    result: 'This route is not yet defined',
+  });
+  return;
+};
+
+exports.createUser = (req, res) => {
+  res.status(505).json({
+    status: 'error',
+    result: 'This route is not yet defined',
+  });
+  return;
+};
+
+exports.updateUser = (req, res) => {
+  res.status(505).json({
+    status: 'error',
+    result: 'This route is not yet defined',
+  });
+  return;
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(505).json({
+    status: 'error',
+    result: 'This route is not yet defined',
+  });
+  return;
+};
