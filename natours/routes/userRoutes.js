d8af9997725c2727eb1049eaf09b097a10c6e043
Koigor97 <turaysamuel101@yahoo.com>
2024-03-09 18:53:28 +0200

    fix bug in code

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const userController = require('../controllers/tourController');
+const userController = require('../controllers/userController');
 const router = express.Router();
 
 router
   .route('/')
-  .get(userController.getAllTour)
-  .post(userController.createTour);
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
   .route('/:id')
-  .get(userController.getTour)
-  .patch(userController.updateTour)
-  .delete(userController.deleteTour);
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
