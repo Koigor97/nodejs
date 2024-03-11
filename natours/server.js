9f8cbaea6c8561d6b0bd472cccb1f43842bbd3fd
Koigor97 <turaysamuel101@yahoo.com>
2024-03-11 12:03:56 +0200

    test Schemas are working

--- natours/server.js
+++ natours/server.js
@@ -7,6 +7,7 @@ const app = require('./app');
 async function makeConnection(dbString) {
   const connection = await mongoose.connect(dbString, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   });
@@ -19,6 +20,38 @@ async function makeConnection(dbString) {
 const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 //connect to database
 const client = makeConnection(db);
+
+// creating a test Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: ['true', 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 5,
+  },
+  price: {
+    type: Number,
+    required: ['true', 'A tour must have a price'],
+  },
+});
+
+// creating a model
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'Sugar Loaf Lagbaja',
+  price: 350,
+});
+
+//saving test document
+// testTour
+//   .save()
+//   .then((doc) => console.log(doc))
+//   .catch((err) => {
+//     console.log(`ERROR...:`, err);
+//   });
 
 ///////////////////////////////////////////////////////////////////////
 // Starting the server
