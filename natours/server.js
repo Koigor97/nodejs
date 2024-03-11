8d1ad98a79797f6c7ab38f4c8647cc73794aec39
Koigor97 <turaysamuel101@yahoo.com>
2024-03-11 13:04:56 +0200

    refactor code

--- natours/server.js
+++ natours/server.js
@@ -21,38 +21,6 @@ const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 //connect to database
 const client = makeConnection(db);
 
-// creating a test Schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: ['true', 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 5,
-  },
-  price: {
-    type: Number,
-    required: ['true', 'A tour must have a price'],
-  },
-});
-
-// creating a model
-const Tour = mongoose.model('Tour', tourSchema);
-const testTour = new Tour({
-  name: 'Sugar Loaf Lagbaja',
-  price: 350,
-});
-
-//saving test document
-// testTour
-//   .save()
-//   .then((doc) => console.log(doc))
-//   .catch((err) => {
-//     console.log(`ERROR...:`, err);
-//   });
-
 ///////////////////////////////////////////////////////////////////////
 // Starting the server
 const port = process.env.PORT || 8030;
