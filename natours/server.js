42f7e5c9e596b1aa44946ca2aca98a42b201d3a5
Koigor97 <turaysamuel101@yahoo.com>
2024-03-11 11:30:04 +0200

    require mongoose and create a connection function

--- natours/server.js
+++ natours/server.js
@@ -1,9 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+// using async function to connect to mongodb
+async function makeConnection(dbString) {
+  const connection = await mongoose.connect(dbString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+
+  console.log('connect to DB successful');
+  return connection;
+}
+
+// getting the database string
+const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+//connect to database
+const client = makeConnection(db);
+
 ///////////////////////////////////////////////////////////////////////
-const port = 8000;
+// Starting the server
+const port = process.env.PORT || 8030;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
