3aae4fbb949a2528d64d0c197fa2407200e1e6d6
Koigor97 <turaysamuel101@yahoo.com>
2024-03-12 13:25:07 +0200

    create file for importing and deleting folder

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const Tour = require('../../models/tourModel');
+
+// using async function to connect to mongodb
+async function makeConnection(dbString) {
+  const connection = await mongoose.connect(dbString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+
+  console.log('connect to DB successful');
+  return connection;
+}
+
+// getting the database string
+const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+//connect to database
+const client = makeConnection(db);
+
+///////////////////////////////////////////////////////////////////////
+// Starting the server
+const port = process.env.PORT || 8030;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+// import data into db
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// deleting all data from db
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successful');
+  } catch (err) {
+    console.log(err);
+  }
+};
