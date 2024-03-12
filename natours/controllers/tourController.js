d866349f337ab23cb84093ce89a9aa9913c798f0
Koigor97 <turaysamuel101@yahoo.com>
2024-03-12 23:19:41 +0200

    create aliasTopTour request-response func

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary';
+  next();
+};
+
 // handling request and response for all tours
 exports.getAllTour = async (req, res) => {
   const queryObj = { ...req.query };
