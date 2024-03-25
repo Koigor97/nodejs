15ba9c72e681f188dfdf15ba9d46010c38a3e6e7
Koigor97 <turaysamuel101@yahoo.com>
2024-03-25 20:06:49 +0200

    add a get request

--- express/expressServer.js
+++ express/expressServer.js
@@ -1,24 +1,38 @@
+const path = require("path");
+const fs = require("fs");
+
+//////////////////////////////////
+const express = require("express");
 // NODEJS is the language
 // Express is node, a node module
-const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 5055;
 
-// '.all' is a method, and it takes 2 args:
-// 1. route
-// 2. callback to run if the route is requested
-app.all("*", (req, res) => {
-  // Express handles the basic headers (status code, mime-type) ! Awesome
-  res.send("<h1> This is the Home page </h1>");
-  // Express also handles the end().
-});
+const getAllTours = () => {
+  const filePath = path.join(`${__dirname}`, "data", "test.json");
+  const file = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(file);
+  return data;
+};
 
+const tours = getAllTours();
 // the app instance has few methods:
 // 1. get() | GET -> the default for a browser
 // 2. post() | POST | CREATE
 // 3. delete() | DELETE
 // 4. put() | PUT | UPDATE (a new document or data)
 // 5. patch() | PATCH | UPDATE (update parts of the already saved document)
+
+app.get("/api/v1/tours", (req, res) => {
+  // Express handles the basic headers (status code, mime-type)
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    tours,
+  });
+  // Express also handles the end().
+});
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
